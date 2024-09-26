3a985aeaacb7d54056b22bdd149762c2679f060e
Shakirullah_304 <shakir.edu304@gmail.com>
2024-09-26 12:42:19 -0700

    add event listner for button click

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
 var addBtn = document.getElementById("addBtn");
 var todoInput = document.getElementById("inputField");
 var todoList = document.getElementById("list");
+
+addBtn.addEventListener("click", addTodo);
+
+function addTodo() {
+    let taskText = todoInput.value;
+}
